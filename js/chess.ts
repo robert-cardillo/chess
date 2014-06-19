2b52c0c881808e53f192f2e60f78323a745bc08b
Snow Blind <snowy.tmp@gmail.com>
2014-06-19 18:21:20 +0300

    Paydos..

--- js/chess.ts
+++ js/chess.ts
@@ -3,22 +3,51 @@ class Chess {
 	PIXEL_SCALE : number = 75;
 	isWhite : boolean = true;
 
-	resetBoard($board) {
+	initBoard($board) {
+		var pieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+
 		$board.empty();
 
 		for (var i = 0; i < 8; i++) {
 			for (var j = 0; j < 8; j++) {
+				var left = i * this.PIXEL_SCALE + 'px';
+				var top = j * this.PIXEL_SCALE + 'px';
+				
 				$('<div/>', {
 					'class' : ((i + j) % 2 == 1 ? 'black' : 'white') + ' square'
 				})
 				.css({
-					'left' : i * this.PIXEL_SCALE + 'px',
-					'top' : j * this.PIXEL_SCALE + 'px'
+					'left' : left,
+					'top' : top
 				})
 				.appendTo($board);
+				
+				if (j == 0 || j == 7) {
+					$('<div/>', {
+						'class' : 'piece ' + (j == 0 ? 'black' : 'white') + ' ' + pieces[i]
+					})
+					.css({
+						'left' : left,
+						'top' : top
+					})
+					.appendTo($board);
+				}
+				
+				if (j == 1 || j == 6) {
+					$('<div/>', {
+						'class' : 'piece ' + (j == 1 ? 'black' : 'white') + ' pawn'
+					})
+					.css({
+						'left' : left,
+						'top' : top
+					})
+					.appendTo($board);
+				}
 			}
 		}
 	}
 }
 
 var app = new Chess();
+
+app.initBoard($('#board'));
